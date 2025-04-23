3169a96dfc312309c74d0dbb7901310976abd0d1
Sonadi <144614600+Sonadi@users.noreply.github.com>
2025-04-23 18:53:58 +0530

    add

---
+++ Payment-Service/Controllers/CheckoutController.cs
@@ -0,0 +1,41 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using System.Runtime.CompilerServices;
+//using System.Text;
+
+//namespace Payment_Service.Controllers
+//{
+//    public class CheckoutController : Controller
+//    {
+
+//        private string Paypalclient { get; set; } = "";
+//        private string Paypalsecret { get; set; } = "";
+//        private string Paypalurl { get; set; } = "";
+
+//        public CheckoutController(IConfiguration configuration)
+//        {
+//            Paypalclient = configuration["PayPal:ClientId"];
+//            Paypalsecret = configuration["PayPal:ClientSecret"];
+//            Paypalurl = configuration["PayPal:Url"]!;
+//        }
+
+//        public IActionResult Index() 
+//        {
+
+//            return View();
+//        }
+
+//        private string GetPaypalAccessToken() { 
+//            string accessToken = "";
+
+//            string credentials64 =
+//                Convert.ToBase64String(Encoding.UTF8.GetBytes(PaypalClientId + ":" + PaypalSecret));
+
+//            client.DefaultRequestHeaders.Add("Authorization", "Basic" + credentials64);
+
+//            var requestMessage = new HttpRequestMessage(HttpMethod.Post, url);
+//            requestMessage.Content = newmStringContent("grant_type=")
+
+//            return accessToken;
+//        }
+//    }
+//}
--- Payment-Service/obj/Debug/net8.0/Payment-Service.AssemblyInfo.cs
+++ Payment-Service/obj/Debug/net8.0/Payment-Service.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Payment-Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de29d83b61d3a071a02c376179b66a5ce9b295f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d663246cc3084633d0f4763fe35bd112700b32c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Payment-Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Payment-Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
