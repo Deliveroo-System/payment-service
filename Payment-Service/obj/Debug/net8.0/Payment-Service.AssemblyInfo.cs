024276cb2dfdf5994fbc83e4e742ee5b5d6b25ea
Sonadi <144614600+Sonadi@users.noreply.github.com>
2025-04-26 13:38:28 +0530

    add

--- Payment-Service/obj/Debug/net8.0/Payment-Service.AssemblyInfo.cs
+++ Payment-Service/obj/Debug/net8.0/Payment-Service.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Payment-Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3169a96dfc312309c74d0dbb7901310976abd0d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c3cdefb292da2db2f1c5ee641f2a93e4b1c676")]
 [assembly: System.Reflection.AssemblyProductAttribute("Payment-Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Payment-Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
